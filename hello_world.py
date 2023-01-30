20f58e800569ff61a2ace7ce44700361b7819069
Douggy Park <douggy.park@yahoo.com>
2023-01-30 13:34:01 -0800

    lower model

--- hello_world.py
+++ hello_world.py
@@ -7,10 +7,11 @@ def open_file(filepath) :
 
 openai.api_key=open_file('openaiapikey.txt')
 
-def gpt3_completion(prompt, engine='text-davinci-002', temp=0.7, top_p = 1.0, tokens =400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']) :
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.7, top_p = 1.0, tokens =400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']) :
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
     response = openai.Completion.create(
         engine=engine,
+        #engine='text-ada-001', 
         prompt=prompt,
         temperature=temp,
         max_tokens=tokens,
